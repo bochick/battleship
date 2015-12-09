82596d8e00acc3ce98f6e09322971ed51702d8f1
JohnMillerBRZ <johnmillerbrz@gmail.com>
2015-12-08 19:31:15 -0500

    SeaGrid

---
+++ SeaGrid.java
@@ -0,0 +1,45 @@
+package battleship;
+
+/**
+ *
+ * @author 
+ */
+public class SeaGrid {
+    
+    private final int SIZE = 10;
+    private char[][] grid = new char[SIZE][SIZE];
+    String name;
+    
+    private final char open = '^';
+    
+    public SeaGrid(String nameIn) {
+        name = nameIn;
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                grid[i][j] = open;
+            }
+        }
+    }
+    
+    public char getSquare(int row, int column) {
+        return grid[row][column];
+    }
+    
+    public void update(int row, int column, char shell) {
+        grid[row][column] = shell;
+    }
+    
+    public String toString() {
+        String result = name + "\n0\t1\t2\t3\t4\t5\t6\t7\t8\t9\t10\n";
+
+        for (int row=0; row < grid.length; row++)
+        {
+            result += (row + 1) + "\t";
+            for (int column=0; column < grid[row].length; column++)
+                result += grid[row][column] + "\t";
+            result += "\n";
+        }
+
+        return result;
+    }
+}
