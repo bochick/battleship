2160ae04374cddbba557d55c49946cdaaf7f6d8f
bochick <bochick0@gmail.com>
2015-12-09 14:30:49 -0500

    Update BattleShipGame.java

--- BattleShipGame.java
+++ BattleShipGame.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package battleship;
 
 import java.util.Scanner;
@@ -31,6 +26,45 @@ public class BattleShipGame {
         player.buildGrid();
         ai.buildGrid();
         
-        player.attack(ai);
+        
+        
+        do
+        {
+           
+            playerTurn();
+            aiTurn();
+       
+        }while(!player.fleetSunk() && !ai.fleetSunk());
+    }
+    
+    private void playerTurn()
+    {
+       System.out.println(player.getName() + "'s Personal Grid:" 
+               + player.getPersonal() );
+       System.out.println("\n--------------------------------------------------"
+               + "----------\n\n" + player.getName() + "'s Target Grid:"
+       + player.getGuess());
+       int[] coordinates = player.attack();
+       char shot = ai.hitOrMiss(coordinates[0], coordinates[1]);
+        if( shot != '^' )
+        {
+            player.GoodHit(coordinates[0], coordinates[1]);
+            ai.BadHit(coordinates[0], coordinates[1]);
+            System.out.println("You've landed a shot!");
+            
+        }
+        
+    }
+    
+    private void aiTurn()
+    {
+        int[] coordinates = ai.attack();
+        char shot = ai.hitOrMiss(coordinates[0], coordinates[1]);
+        
+        if( shot != '^')
+        {
+            ai.GoodHit(coordinates[0], coordinates[1]);
+            player.BadHit(coordinates[0], coordinates[1]);
+        }
     }
 }
