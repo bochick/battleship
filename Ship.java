4e51a2cc88f3e16fd3c1f8bd94221519b3bca4e9
JohnMillerBRZ <johnmillerbrz@gmail.com>
2015-12-08 19:31:49 -0500

    Create Ship.java

---
+++ Ship.java
@@ -0,0 +1,62 @@
+package battleship;
+
+/**
+ * @author John Miller
+ */
+public class Ship {
+    private String name;
+    private int size;
+    private int hits;
+    private int direction;
+    private int[] location;
+    
+    public Ship (String nameIn, int sizeIn) {
+        name = nameIn;
+        size = sizeIn;
+        hits = 0;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public int getSize() {
+        return size;
+    }
+    
+    public int[] getLocation() { 
+        return location;
+    }
+    
+    public int getDirection() {
+        return direction;
+    }
+    
+    public void setLocation(int x, int y) {
+        location[0] = x;
+        location[1] = y;
+    }
+    
+    public void setDirection(int dir) {
+        direction = dir;
+    }
+    
+    public void hit() {
+        hits--;
+    }
+    
+    public boolean isSunk() {
+        if (size == hits)
+            return true;
+        
+        return false;
+    }
+    
+    public char title() {
+        return name.charAt(0);
+    }
+    
+    public String toString() {
+        return name;
+    }
+}
