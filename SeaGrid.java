24a28e80020c1dc2012d4312e3a49c1a408952d3
JohnMillerBRZ <johnmillerbrz@gmail.com>
2015-12-10 19:37:55 -0500

    Update SeaGrid.java

--- SeaGrid.java
+++ SeaGrid.java
@@ -7,12 +7,9 @@ package battleship;
 public class SeaGrid {
     
     private final int SIZE = 10;
-    private static final String LETTER[]
-            = {"A","B","C","D","E","F","G","H","I","J"};
     private char[][] grid = new char[SIZE][SIZE];
-    String name;
-    
     private final char open = '^';
+    String name;
     
     public SeaGrid(String nameIn) {
         name = nameIn;
@@ -28,22 +25,18 @@ public class SeaGrid {
         grid[row][col] = mark; 
     }
     
-    public char getSquare(int row, int column) {
-        return grid[row][column];
-    }
-    
-    public void update(int row, int column, char shell) {
-        grid[row][column] = shell;
+    public char getSquare(int row, int col) {
+        return grid[row][col];
     }
     
     public String toString() {
         String result = name + "\n0\t1\t2\t3\t4\t5\t6\t7\t8\t9\t10\n";
 
-        for (int row=0; row < grid.length; row++)
+        for (int row=0; row < SIZE; row++)
         {
-            result += LETTER[(row)] + "\t";
-            for (int column=0; column < grid[row].length; column++)
-                result += grid[row][column] + "\t";
+            result += (row + 1) + "\t";
+            for (int col=0; col < SIZE; col++)
+                result += grid[row][col] + "\t";
             result += "\n";
         }
 
