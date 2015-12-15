81c6c9f4bdd54b3d95a4870829f9701f8d68661a
JohnMillerBRZ <johnmillerbrz@gmail.com>
2015-12-15 11:25:43 -0500

    Update AiPlayer.java

--- AiPlayer.java
+++ AiPlayer.java
@@ -13,18 +13,17 @@ import java.util.Random;
  */
 public class AiPlayer extends Player {
 
-     private Ship[] fleet;
+    private Ship[] fleet;
     private Random rand = new Random();
     private SeaGrid personalGrid;
     private SeaGrid targetGrid = new SeaGrid("Target grid");
 
-   public enum Direction {up, down, left, right}
     private int[] lastAttack = new int[2];
     private int[] temp = new int[2];
     private int numConsHits = 0;//remembers the number of hits in a row
     private int adjacentAttacks;
     private Direction prevDirection;//remembers the previous direction
-    
+
     private boolean multiHits = false;//if true, continue attack pattern
     private boolean lastAttackHit;
 
@@ -40,29 +39,30 @@ public class AiPlayer extends Player {
         fleet[3] = new Ship("Cruiser", 4);
         fleet[4] = new Ship("Aircraft Carrier", 5);
     }
-   
-     @Override
-    public void buildGrid(){
-       randomPlacement();
+
+    @Override
+    public void buildGrid() {
+        randomPlacement();
     }
+
     //-------------------------------------------------
     //attack() returns the cordinates of the ai's attack
     //if the ai has no previous hits, attack at random
     //otherwise call nextAttack() if the last attack hit
     //-------------------------------------------------
+
     @Override
-    public int[] attack(){
+    public int[] attack() {
         int row = rand.nextInt(9);
         int col = rand.nextInt(9);
         int[] currentAttack = {row, col};
-        
-        if(lastAttackHit){
+
+        if (lastAttackHit) {
             adjacentAttacks++;
             System.out.println("ai got into lastattackhit, multihits is " + multiHits);
             System.out.println("num of cons hits: " + numConsHits);
             return nextAttack();
-        }
-        else{
+        } else {
             System.out.println("ai did not get into lastattackhit");
             return lastAttack = currentAttack;
         }
@@ -76,94 +76,70 @@ public class AiPlayer extends Player {
     //3 = right
     //is up by default
     //-------------------------------------------------
-   private Direction choseRandomDirection(){
-       int randDirection = rand.nextInt(4);
-       switch (randDirection) {
-                case 0: {
-                   return Direction.up; 
-                }
-                case 1: {
-                    return Direction.down;
-                }
-
-                case 2: {
-                    return Direction.left;
-                }
-                case 3: {
-                    return Direction.right;
-                }
-                default:{
-                    return Direction.up;
-                }
+    private Direction choseRandomDirection() {
+        int randDirection = rand.nextInt(4);
+        switch (randDirection) {
+            case 0: {
+                return Direction.up;
             }
+            case 1: {
+                return Direction.down;
+            }
+
+            case 2: {
+                return Direction.left;
+            }
+            case 3: {
+                return Direction.right;
+            }
+            default: {
+                return Direction.up;
+            }
+        }
     }
+
     //-------------------------------------------------
     //used to get the inverse of the current direction
     //-------------------------------------------------
-   private Direction inverse(Direction current){
-       switch (current) {
-           case up: {
-                   return Direction.down; 
-                }
-                case down: {
-                    return Direction.up;
-                }
 
-                case left: {
-                    return Direction.right;
-                }
-                case right: {
-                    return Direction.left;
-                }
-                default:{
-                    return current;
-                }
+    private Direction inverse(Direction current) {
+        switch (current) {
+            case up: {
+                return Direction.down;
             }
-   }
+            case down: {
+                return Direction.up;
+            }
+
+            case left: {
+                return Direction.right;
+            }
+            case right: {
+                return Direction.left;
+            }
+            default: {
+                return current;
+            }
+        }
+    }
+
     //--------------------------------------------------------------------------
     //nextAttack returns the cordinates of the next attack ai will make
     //also sets multiHits to true if ai has landed atleast 2 attacks in a row
     //checks if the direction is valid on the grid, if not inverse the direction
     //--------------------------------------------------------------------------
-   private int[] nextAttack(){
-       temp = lastAttack;
-       if(numConsHits >= 2)
-           multiHits = true;
-       
-       if(multiHits){
-           switch (prevDirection) {
-                case up: {
-                   lastAttack[0]--;
-                   prevDirection = Direction.up;
-                }
-                case down: {
-                   lastAttack[0]++;
-                    prevDirection = Direction.down;
-                }
 
-                case left: {
-                    lastAttack[1]--;
-                    prevDirection = Direction.left;
-                }
-                case right: {
-                    lastAttack[1]++;
-                     prevDirection = Direction.right;
-                }
-            }
-           if(isValidAttack(lastAttack[0], lastAttack[1]))
-                return lastAttack;
-           else{
-               prevDirection = inverse(prevDirection);
-               return nextAttack();
-            }
-       }
-       else{
-           Direction tempDir = choseRandomDirection();
-           System.out.println(tempDir);
-           switch (tempDir) {
+    private int[] nextAttack() {
+        temp = lastAttack;
+        if (numConsHits >= 2) {
+            multiHits = true;
+        }
+
+        if (multiHits) {
+            switch (prevDirection) {
                 case up: {
-                   lastAttack[0]--;
-                   prevDirection = Direction.up;
+                    lastAttack[0]--;
+                    prevDirection = Direction.up;
                 }
                 case down: {
                     lastAttack[0]++;
@@ -176,16 +152,45 @@ public class AiPlayer extends Player {
                 }
                 case right: {
                     lastAttack[1]++;
-                     prevDirection = Direction.right;
+                    prevDirection = Direction.right;
                 }
-            } 
-           if(isValidAttack(lastAttack[0], lastAttack[1]))
-                return lastAttack;
-           else{
-               return nextAttack();
             }
-       }
-   }
+            if (isValidAttack(lastAttack[0], lastAttack[1])) {
+                return lastAttack;
+            } else {
+                prevDirection = inverse(prevDirection);
+                return nextAttack();
+            }
+        } else {
+            Direction tempDir = choseRandomDirection();
+            System.out.println(tempDir);
+            switch (tempDir) {
+                case up: {
+                    lastAttack[0]--;
+                    prevDirection = Direction.up;
+                }
+                case down: {
+                    lastAttack[0]++;
+                    prevDirection = Direction.down;
+                }
+
+                case left: {
+                    lastAttack[1]--;
+                    prevDirection = Direction.left;
+                }
+                case right: {
+                    lastAttack[1]++;
+                    prevDirection = Direction.right;
+                }
+            }
+            if (isValidAttack(lastAttack[0], lastAttack[1])) {
+                return lastAttack;
+            } else {
+                return nextAttack();
+            }
+        }
+    }
+
     //-------------------------------------------------
     //***** to be used in battleShipGame ******
     //*****************************************
@@ -193,75 +198,83 @@ public class AiPlayer extends Player {
     //used to change the direction if we know the orientation
     //but still miss
     //-------------------------------------------------
-   public void changeDirection(){
-       if(!hitOrMiss(lastAttack[0], lastAttack[1]) && multiHits)
-           prevDirection = inverse(prevDirection);
-   }
-   
-   public void setLastAttackHit(boolean hit){
-       lastAttackHit = hit;
-   }
+
+    public void changeDirection() {
+        if (!hitOrMiss(lastAttack[0], lastAttack[1]) && multiHits) {
+            prevDirection = inverse(prevDirection);
+        }
+    }
+
+    public void setLastAttackHit(boolean hit) {
+        lastAttackHit = hit;
+    }
+
     //-------------------------------------------------
     //checks to make sure the attack is within the grid
     //-------------------------------------------------
-   private boolean isValidAttack(int row, int col){
-       if(row < 10 || col < 10)
-           return true;
-       else
-           return false;
-                   
-   }
+
+    private boolean isValidAttack(int row, int col) {
+        if (row < 10 || col < 10) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
    //-----------------------------------------
-   //***** to be used in battleShipGame ******
-   //*****************************************
+    //***** to be used in battleShipGame ******
+    //*****************************************
+    //-----------------------------------------
+    //checks if the ai has landed multiple hits
+    //-----------------------------------------
+
+    public boolean hasMultiHits() {
+        return multiHits;
+    }
+
    //-----------------------------------------
-   //checks if the ai has landed multiple hits
+    //***** to be used in battleShipGame ******
+    //*****************************************
+    //-----------------------------------------
+    //chekcs the number of adjacent attacks
+    //used so the ai will check all 4 adjacent
+    //squares in the worse case
+    //-----------------------------------------
+    public int getAdjacentAttacks() {
+        return adjacentAttacks;
+    }
    //-----------------------------------------
-   public boolean hasMultiHits(){
-       return multiHits;
-   }
-   
-   
+    //***** to be used in battleShipGame ******
+    //*****************************************
+    //-----------------------------------------
+    //used to add to or reset the number of
+    //cons hits
+    //-----------------------------------------
+
+    public void setNumConsHits(int n) {
+        numConsHits = n;
+    }
    //-----------------------------------------
-   //***** to be used in battleShipGame ******
-   //*****************************************
+    //***** to be used in battleShipGame ******
+    //*****************************************
+    //-----------------------------------------
+    //used to get the current number of cons hits
+    //-----------------------------------------
+
+    public int getNumConsHits() {
+        return numConsHits;
+    }
    //-----------------------------------------
-   //chekcs the number of adjacent attacks
-   //used so the ai will check all 4 adjacent
-   //squares in the worse case
-   //-----------------------------------------
-   public int getAdjacentAttacks(){
-       return adjacentAttacks;
-   }
-   //-----------------------------------------
-   //***** to be used in battleShipGame ******
-   //*****************************************
-   //-----------------------------------------
-   //used to add to or reset the number of
-   //cons hits
-   //-----------------------------------------
-   public void setNumConsHits(int n){
-       numConsHits = n;
-   }
-   //-----------------------------------------
-   //***** to be used in battleShipGame ******
-   //*****************************************
-   //-----------------------------------------
-   //used to get the current number of cons hits
-   //-----------------------------------------
-   public int getNumConsHits(){
-       return numConsHits;
-   }
-   //-----------------------------------------
-   //***** to be used in battleShipGame ******
-   //*****************************************
-   //-----------------------------------------
-   //used to reset last attack after missing
-   //when attacking an adjacent square. only
-   //up to 4 times
-   //-----------------------------------------
-   public void resetLastAttack(){
-       lastAttack = temp;
-   }
-   
+    //***** to be used in battleShipGame ******
+    //*****************************************
+    //-----------------------------------------
+    //used to reset last attack after missing
+    //when attacking an adjacent square. only
+    //up to 4 times
+    //-----------------------------------------
+
+    public void resetLastAttack() {
+        lastAttack = temp;
+    }
+
 }
