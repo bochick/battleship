8d1cb0edd0a32538957c470a1127153c002d40b5
JohnMillerBRZ <johnmillerbrz@gmail.com>
2015-12-16 20:19:01 -0500

    Update Ship.java

--- Ship.java
+++ Ship.java
@@ -4,21 +4,16 @@ package battleship;
  * @author John Miller
  */
 public class Ship {
-    public enum Enum {Destroyer, Submarine, Frigate, Cruiser, Aircraft_Carrier}
-    
     private String name;
     private int size;
     private int hits;
     private int direction;
     private int[] location = new int[2];
-    private Enum enValue;
     
     public Ship (String nameIn, int sizeIn) {
         name = nameIn;
         size = sizeIn;
         hits = 0;
-        
-        enValue = Enum.valueOf(name);
     }
     
     public String getName() {
@@ -38,8 +33,8 @@ public class Ship {
     }
     
     public void setLocation(int row, int col) {
-        location[0] = row;
-        location[1] = col;
+        location[1] = row;
+        location[0] = col;
     }
     
     public void setDirection(int dir) {
@@ -59,10 +54,6 @@ public class Ship {
     
     public char getTitle() {
         return name.charAt(0);
-    }
-    
-    public Enum getEnum() {
-        return enValue;
     }
     
     public int getHits() {
